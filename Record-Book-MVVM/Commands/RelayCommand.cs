f80ad6b0e672db1deea569ff26f1d3a47d01bec3
Ali Zeinali <azeinali@insys-test.de>
2023-06-01 10:11:31 +0200

    Add project files.

---
+++ Record-Book-MVVM/Commands/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Record_Book_MVVM.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public Action<object> _Execute { get; set; } // Returns Void
+        public Predicate<object> _CanExecute { get; set; } //returns Boolean
+        public RelayCommand() { 
+        
+        }
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Record-Book-MVVM/Models/UserManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Record_Book_MVVM.Models
+{
+    public class UserManager
+    {
+        public UserManager() { }
+        public static ObservableCollection<User> _DatabaseUsers = new ObservableCollection<User>()
+        {
+            new User(){Name="Ali", Email="azeinali@insys-test.de"}
+        };
+
+        public static ObservableCollection<User> _GetUsers()
+        {
+            return _DatabaseUsers;
+        }
+
+        public static void AddUser(User user)
+        { 
+            _DatabaseUsers.Add(user);
+        }
+    }
+}
